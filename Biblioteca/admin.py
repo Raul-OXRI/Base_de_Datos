4979774d95cc293c19726d2c3e8a431a1459fb9b
Raul-OXRI <jose.botzoc.30@gmail.com>
2023-10-21 03:10:05 -0600

    :fire: Se creo la parte de ejemplares donde se hizo modificaiones tanto como las llaves foraneas y algunos campos, de igual se comenzara a modificar la ida principal de base de datos

--- Biblioteca/admin.py
+++ Biblioteca/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import Biblioteca,Encargado
 
+def get_encargado(obj):
+    return ', '.join([encargado.Nombre for encargado in obj.Encargado.all()])
 
 class BibliotecaAdmin(admin.ModelAdmin):
-    list_display = ('Nombre', 'Ubicacion', 'Region', 'Encargado')
+    list_display = ('Nombre', 'Ubicacion', 'Region', get_encargado)
     #readonly_fields=('Created' , 'Updated')
 
 admin.site.register(Encargado)
--- Biblioteca/models.py
+++ Biblioteca/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from .choices import Region
-from django.core.validators import RegexValidator
 
 
 class Encargado(models.Model):
@@ -10,8 +9,6 @@ class Encargado(models.Model):
     Telefono = models.CharField(max_length=13,default='+502 ',help_text='El número de teléfono debe tener 8 dígitos,sin espacios.',)
     Correo_electronico = models.EmailField()
     Contrasena = models.CharField(max_length=100)
-    #Created = models.DateTimeField(auto_now_add=True)
-    #Updated = models.DateTimeField(auto_now_add=True)
     class Meta:
         verbose_name='encargado'
         verbose_name_plural='encargados'
@@ -25,7 +22,9 @@ class Biblioteca(models.Model):
     Ubicacion = models.CharField(max_length=50)
     Region = models.CharField(max_length=13, choices=Region)
     Horario = models.CharField(max_length=50)
-    Encargado = models.ForeignKey(Encargado, null=True,blank=True, on_delete=models.SET_NULL)
+    #se coloco el campo para seleccionar mas encargado por logica porque puede una biblioteca peude tener 1 omas encaragdos 
+    Encargado = models.ManyToManyField(Encargado,blank=True)
+    #Encargado = models.ForeignKey(Encargado, null=True,blank=True, on_delete=models.SET_NULL)
     Created = models.DateTimeField(auto_now_add=True)
     Updated = models.DateTimeField(auto_now_add=True)
     class Meta:
